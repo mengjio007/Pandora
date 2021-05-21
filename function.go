215395b935669f9388d5ffcd49296b3da1aba879
xuma <1959477235@qq.com>
2021-05-21 11:17:43 +0800

    feat(sort):bubble-quick

---
+++ function.go
@@ -0,0 +1,42 @@
+package Pandora
+
+// 冒泡
+// BubbleSort
+func BubbleSort(values []int) {
+	for i := 0; i < len(values)-1; i++ {
+		for j := i + 1; j < len(values); j++ {
+			if values[i] > values[j] {
+				values[i], values[j] = values[j], values[i]
+			}
+		}
+	}
+}
+
+// 快排
+// QuickSort
+func QuickSort(arr []int, start, end int) {
+	if start < end {
+		i, j := start, end
+		key := arr[(start+end)/2]
+		for i <= j {
+			for arr[i] < key {
+				i++
+			}
+			for arr[j] > key {
+				j--
+			}
+			if i <= j {
+				arr[i], arr[j] = arr[j], arr[i]
+				i++
+				j--
+			}
+		}
+
+		if start < j {
+			QuickSort(arr, start, j)
+		}
+		if end > i {
+			QuickSort(arr, i, end)
+		}
+	}
+}
