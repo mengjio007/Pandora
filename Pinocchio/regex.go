fb783860081ec9d2edf85041d74056f86d9271b3
xuma <1959477235@qq.com>
2021-05-21 15:34:11 +0800

    feat(regex):regex

---
+++ Pinocchio/regex.go
@@ -0,0 +1,60 @@
+package Pinocchio
+
+import "regexp"
+
+const (
+	regexEmailPattern       = `(?i)[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}`
+	regexStrictEmailPattern = `(?i)[A-Z0-9!#$%&'*+/=?^_{|}~-]+` +
+		`(?:\.[A-Z0-9!#$%&'*+/=?^_{|}~-]+)*` +
+		`@(?:[A-Z0-9](?:[A-Z0-9-]*[A-Z0-9])?\.)+` +
+		`[A-Z0-9](?:[A-Z0-9-]*[A-Z0-9])?`
+	regexUrlPattern          = `(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?`
+	regexPhonePattern        = `^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$`
+	regexUserIdentityPattern = `^([1-6][1-9]|50)\d{4}(18|19|20)\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$`
+)
+
+var (
+	regexEmail        *regexp.Regexp
+	regexStrictEmail  *regexp.Regexp
+	regexUrl          *regexp.Regexp
+	regexPhone        *regexp.Regexp
+	regexIdentityCard *regexp.Regexp
+)
+
+func init() {
+	regexEmail = regexp.MustCompile(regexEmailPattern)
+	regexStrictEmail = regexp.MustCompile(regexStrictEmailPattern)
+	regexUrl = regexp.MustCompile(regexUrlPattern)
+	regexPhone = regexp.MustCompile(regexPhonePattern)
+	regexIdentityCard = regexp.MustCompile(regexUserIdentityPattern)
+}
+
+// IsEmail validates string is an email address, if not return false
+// basically validation can match 99% cases
+func IsEmail(email string) bool {
+	return regexEmail.MatchString(email)
+}
+
+// IsEmailRFC validates string is an email address, if not return false
+// this validation omits RFC 2822
+func IsEmailRFC(email string) bool {
+	return regexStrictEmail.MatchString(email)
+}
+
+// IsUrl validates string is a url link, if not return false
+// simple validation can match 99% cases
+func IsUrl(url string) bool {
+	return regexUrl.MatchString(url)
+}
+
+// IsPhone validates string is a chinese phone number, if not return false
+// simple validation can match 99% cases
+func IsPhone(phone string) bool {
+	return regexPhone.MatchString(phone)
+}
+
+// IsIdentityCard validates string is a chinese user ID card, if not return false
+// simple validation can match 99% cases
+func IsIdentityCard(card string) bool {
+	return regexIdentityCard.MatchString(card)
+}
--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.15
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/coocood/freecache v1.1.1
-	github.com/dchest/captcha v0.0.0-20200903113550-03f5f0333e1f // indirect
+	github.com/dchest/captcha v0.0.0-20200903113550-03f5f0333e1f
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
